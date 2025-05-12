6624d056696ab835c450ce1dcfdf4a0c3b0c6577
TaishoEmmoto <luke.wood.et@gmail.com>
2025-05-12 10:58:13 +0900

    関数を定義して条件分岐を見やすくしました

--- Java-Script-Kadai-3/script.js
+++ Java-Script-Kadai-3/script.js
@@ -1,9 +1,12 @@
 // < JSONとJavaScriptオブジェクトの相互変換 >
-//                                (1)JSON文字列をJavaScriptオブジェクトに変換しましょう
+// 宣言
+const zenkakuRegex1 = /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/;
+const zenkakuRegex2 = /[！-\uff5e]/g;
+// (1)JSON文字列をJavaScriptオブジェクトに変換しましょう
 const obj1 = '{"Name":"Taro", "Age":42, "gender": "male"}';
 const jsonChanger = JSON.parse(obj1);
 console.log(jsonChanger);
-//                                (2)JavaScriptオブジェクトをJSON文字列に変換しましょう
+// (2)JavaScriptオブジェクトをJSON文字列に変換しましょう
 const obj2 = { Name: "Taro", Age: 13, gender: "male" };
 const jsChanger = JSON.stringify(obj2);
 console.log(jsChanger);
@@ -15,12 +18,9 @@ const nameInput = document.getElementById("textarea");
 const errorName = document.getElementById("errorName");
 // イベント設置
 nameInput.addEventListener('input', (event) => {
-// 宣言
   const value = event.target.value;
-  const firstRegex = /^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/;
-  const secondRegex = /[！-\uff5e]/g;
 // 条件分岐
-  if (value === "" || !value.match(firstRegex) || value.match(secondRegex)) {
+  if (value === "" || !value.match(zenkakuRegex1) || value.match(zenkakuRegex2)) {
     errorName.style.display = "block";
   } else {
     errorName.style.display = "none";
@@ -33,13 +33,14 @@ const formYear = document.getElementById("year");
 const errorYear = document.getElementById("errorYear");
 // イベント設置
 formYear.addEventListener('input',(event) => {
+  const value = event.target.value;
 // 入力チェック用コンソールログ
-  console.log(event.target.value);
+  console.log(value);
 // もしインプットの中身が空ならば
-  if (event.target.value === "") {
+  if (value === "") {
     errorYear.style.display="block";
 // もしNotNumberなら
-  }else if (isNaN(event.target.value)) {
+  }else if (isNaN(value)) {
     errorYear.style.display="block";
   }else {
     errorYear.style.display="none";
@@ -52,14 +53,16 @@ const emailAdress = document.getElementById("email");
 const errorEmail = document.getElementById("errorEmail");
 // イベント設置
 emailAdress.addEventListener('input',(event) => {
+  const value = event.target.value;
 // 入力チェック用コンソールログ
-  console.log(event.target.value);
+  console.log(value);
+  const emailRegex = /^\w+@\w+\.\w+$/;
 // ここから条件分岐
 // 空 or "@"が0 or 全角 の場合は エラーを表示
-  if (event.target.value === "" ||
-    event.target.value.indexOf("@") === 0 ||
-    event.target.value.match(/^[\u30a0-\u30ff\u3040-\u309f\u3005-\u3006\u30e0-\u9fcf]+$/) || event.target.value.match(/[！-\uff5e]/g) ||
-    !event.target.value.match(/^\w+@\w+\.\w+$/)) {
+  if (value === "" ||
+    value.indexOf("@") === 0 ||
+    value.match(zenkakuRegex1) || value.match(zenkakuRegex2) ||
+    !value.match(emailRegex)) {
     errorEmail.style.display="block";
   }else{
     errorEmail.style.display="none";
@@ -71,9 +74,10 @@ const phoneNumber = document.getElementById("phoneNumber");
 const errorNumber = document.getElementById("errorNumber");
 // イベント設置
 phoneNumber.addEventListener('input',(event) => {
-  if (event.target.value === "" ||
-    isNaN(event.target.value) ||
-    event.target.value.length !== 11) {
+  const value = event.target.value;
+  if (value === "" ||
+    isNaN(value) ||
+    eventvalue.length !== 11) {
       errorNumber.style.display="block";
   }else{
     errorNumber.style.display="none";
