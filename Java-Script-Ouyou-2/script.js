76d1c69595ab7fb796c7007a295caa8c96060d89
TaishoEmmoto <luke.wood.et@gmail.com>
2025-05-22 12:09:25 +0900

    関数,変数,定数並び替え。コンソールと複数のコメントアウトを削除

--- Java-Script-Ouyou-2/script.js
+++ Java-Script-Ouyou-2/script.js
@@ -1,13 +1,23 @@
 window.onload = () => {
-  // 1. 色のボックスを入手
-  let box0 = document.getElementById("box0");
-  let box1 = document.getElementById("box1");
-  let box2 = document.getElementById("box2");
-  let box3 = document.getElementById("box3");
-  // 2. デフォルトの色配列を設定
+  // デフォルトの色配列を設定
   let defultBox = ["red", "blue", "yellow", "pink"];
-  // 3. シャッフル関数を定義
-  function shuffle (array){
+  // 1. 色のボックスを入手
+  const box0 = document.getElementById("box0");
+  const box1 = document.getElementById("box1");
+  const box2 = document.getElementById("box2");
+  const box3 = document.getElementById("box3");
+  // 正解数のPタグ要素取得
+  let result = document.getElementById("result");
+  // リセットボタンを入手
+  const reset = document.getElementById("reset");
+  // 色ボックスを配列にして代入
+  let colorBoxArry = [box0, box1, box2, box3];
+  // マジックナンバーを定数定義
+  const CORRECT_ANSWER_ONE = 1;
+  const CORRECT_ANSWER_TWO = 2;
+  const CORRECT_ANSWER_FOUR = 4;
+  // シャッフル関数を定義
+  const shuffle = (array) => {
     let colorBox = [...array]; // ["red", "blue", "yellow", "pink"]
     for (let i = defultBox.length -1; i >= 0; i--) {
       let random = Math.floor(Math.random() * (i + 1));
@@ -19,132 +29,112 @@ window.onload = () => {
       }
     return colorBox;
   }
-  // 4. 上下の配列シャッフル処理を定義
+  // 上下の配列シャッフル処理を定義
   // 上の色のシャッフルの処理を定義
   let newColor = shuffle(defultBox);
   // 下の色のシャッフルの処理を定義
   let newSecret = shuffle(defultBox);
-  // 5. 一致しているインデックスを返す関数
-  function checkSameNum() {
+  // 一致しているインデックスを返す関数を定義
+  const checkSameNum = (color1, color2) => {
     let result = [];
     for (let i = 0; i <= 3; i++) {
-      if (newColor[i] === newSecret[i]) {
+      if (color1[i] === color2[i]) {
         result.push(i);
       }
     }
     return result;
   }
-  // 6. 正解数のPタグの要素取得
-  let result = document.getElementById("result");
-  let sameNumArray = checkSameNum(); // 5. の関数を取得
-  // 7. 正解数を表示の関数を定義
-  function resultReroad (){
-    let sameNumArray = checkSameNum();
+  let sameNumarray = checkSameNum(newColor, newSecret);
+  // 正解数を表示の関数を定義
+  const resultReroad = (color1, color2) => {
+    let sameNumArray = checkSameNum(color1, color2);
     result.textContent = `${sameNumArray.length}個正解してます`;
   };
-  resultReroad();//実行
-  // 8. 色ボックスを配列にして代入
-  let colorBoxArry = [box0, box1, box2, box3];
-  // 9. ボックスの色移動の関数を定義
-  function reflectionColor() {
+  // ボックスの色移動の関数を定義
+  let reflectionColor = (color1, color2) => {
     // 上の色を表示
     for (let i = 0; i < colorBoxArry.length; i++) {
       colorBoxArry[i].style.backgroundColor = newColor[i];
     }
   }
-  // 10. 答えがゼロになるように完全シャッフルする関数を定義
-  function zeroResult(){
+  // 色移動反映
+  reflectionColor(newColor, newSecret);
+  // 答えがゼロになるように完全シャッフルする関数を定義
+  let zeroResult = (color1, color2) =>{
+    let sameNumArray = checkSameNum(newColor, newSecret);
+    // 1つが同じ 場合
+    if (sameNumArray.length === CORRECT_ANSWER_ONE){
+      let sameNumArray = checkSameNum(newColor, newSecret);
+      // Indexが0 または Indexが3
+      if (sameNumArray[0] === 0 || sameNumArray[0] === 3){
+        [newSecret[0],newSecret[3]] = [newSecret[3],newSecret[0]];
+      }else if(sameNumArray[0] === 1 || sameNumArray[0] === 2){
+        [newSecret[1],newSecret[2]] = [newSecret[2],newSecret[1]];
+        resultReroad(newColor, newSecret); //正解数反映
+        reflectionColor(newColor, newSecret);//色移動反映
+      }
+    }
     // // 2つが同じ場合
-    if (sameNumArray.length === 2){
-      sameNumArray = checkSameNum();// 5. の関数を取得
-      let numIndex21 = sameNumArray[0];//二つの場合のインデックスの[0]の宣言
-      let numIndex22 = sameNumArray[1];//二つの場合のインデックスの[1]の宣言
-      [newSecret[numIndex21],newSecret[numIndex22]] = [newSecret[numIndex22],newSecret[numIndex21]];
-      resultReroad(); //正解数反映
-      reflectionColor();//色移動反映
-      // console.log("2つが同じ場合");
+    if (sameNumArray.length === CORRECT_ANSWER_TWO){
+      let sameNumArray = checkSameNum(newColor, newSecret);
+      let indexNum0 = sameNumArray[0];//二つの場合のインデックスの[0]の宣言
+      let indexNum1 = sameNumArray[1];//二つの場合のインデックスの[1]の宣言
+      [newSecret[indexNum0],newSecret[indexNum1]] = [newSecret[indexNum1],newSecret[indexNum0]];
+      resultReroad(newColor, newSecret); //正解数反映
+      reflectionColor(newColor, newSecret);//色移動反映
     }
     // 4つが同じ場合
-    if (sameNumArray.length === 4){
-      sameNumArray = checkSameNum();// 5. の関数を取得
+    if (sameNumArray.length === CORRECT_ANSWER_FOUR){
+      let sameNumArray = checkSameNum(newColor, newSecret);
       let numIndex4 = sameNumArray[0];
       let secretTemp2 = newSecret[sameNumArray[0]];
       newSecret.splice(numIndex4, 1); //削除
       newSecret.push(secretTemp2); //後ろに追加
-      resultReroad(); //正解数反映
-      reflectionColor();//色移動反映
-      // console.log("4つが同じ場合");
-    }
-    // 1つが同じ 場合
-    if (sameNumArray.length === 1){
-      sameNumArray = checkSameNum();// 5. の関数を取得
-      // Indexが0 または Indexが3
-      if (sameNumArray[0] === 0 || sameNumArray[0] === 3){
-        [newSecret[0],newSecret[3]] = [newSecret[3],newSecret[0]];
-        // console.log("ひとつが同じで Indexが0 or Indexが3");
-      }else if(sameNumArray[0] === 1 || sameNumArray[0] === 2){
-        [newSecret[1],newSecret[2]] = [newSecret[2],newSecret[1]];
-        resultReroad(); //正解数反映
-        reflectionColor();//色移動反映
-        // console.log("ひとつが同じで Indexが1 or Indexが2");
-      }
+      resultReroad(newColor, newSecret); //正解数反映
+      reflectionColor(newColor, newSecret);//色移動反映
     }
   }
-  zeroResult(); //正解数反映
-  reflectionColor(); //色移動反映
-  // 11. ボタン移動のイベント
-  // // 0 と 1 入れ替え
+  zeroResult(newColor, newSecret); //正解数反映
+  resultReroad(newColor, newSecret); //正解数反映
+  // console.log(newSecret,newColor);
+  // ボタン移動のイベント
+  // 0 と 1 入れ替え
   document.querySelectorAll(".change01").forEach(change01 => {
     change01.addEventListener('click', () => {
-      sameNumArray = checkSameNum();// 5. の関数を取得
+      checkSameNum(newColor, newSecret);
       [newColor[0],newColor[1]] = [newColor[1],newColor[0]];
-      resultReroad(); //正解数反映
-      reflectionColor();//色移動反映
-      console.log(newColor);
-      console.log(newSecret);
+      resultReroad(newColor, newSecret); //正解数反映
+      reflectionColor(newColor, newSecret);//色移動反映
+      // console.log(newColor,newSecret);
     });
   });
-  // // 1 と 2 入れ替え
+  // 1 と 2 入れ替え
   document.querySelectorAll(".change12").forEach(change12 => {
     change12.addEventListener('click', () => {
-      sameNumArray = checkSameNum();// 5. の関数を取得
+      checkSameNum(newColor, newSecret);
       [newColor[1],newColor[2]] = [newColor[2],newColor[1]];
-      resultReroad(); //正解数反映
-      reflectionColor();//色移動反映
-      console.log(newColor);
-      console.log(newSecret);
+      resultReroad(newColor, newSecret); //正解数反映
+      reflectionColor(newColor, newSecret);//色移動反映
+      // console.log(newColor,newSecret);
     });
   });
-  // // 2 と 3 入れ替え
+  // 2 と 3 入れ替え
   document.querySelectorAll(".change23").forEach(change23 => {
     change23.addEventListener('click', () => {
-      sameNumArray = checkSameNum();// 5. の関数を取得
+      checkSameNum(newColor, newSecret);
       [newColor[2],newColor[3]] = [newColor[3],newColor[2]];
-      resultReroad(); //正解数反映
-      reflectionColor();//色移動反映
-      console.log(newColor);
-      console.log(newSecret);
+      resultReroad(newColor, newSecret); //正解数反映
+      reflectionColor(newColor, newSecret);//色移動反映
+      // console.log(newColor,newSecret);
     });
   });
-  resultReroad(); //正解数反映
-  // 12. 答えをコンソールに表示
-  console.log(newColor); //上の色
-  console.log(newSecret); //下の色
-  console.log(sameNumArray);// 10.の完全シャッフル前の被ったインデックスを表示
-  // 13. リセットボタンを入手
-  const reset = document.getElementById("reset");
-  // 14. リセットボタンの処理
+  // リセットボタンの処理
   reset.addEventListener('click', () => {
     // シャッフル
     newColor = shuffle(defultBox);
     newSecret = shuffle(defultBox);
-    sameNumArray = checkSameNum();// 5. の関数を取得
-    zeroResult();
-    resultReroad () //正解数反映
-    reflectionColor();//色移動反映
-    // 答えをコンソールに表示
-    console.log(newColor);//上の色
-    console.log(newSecret);//下の色
-    console.log(sameNumArray);// 10.の完全シャッフル前の被ったインデックスを表示
+    zeroResult(newColor, newSecret);
+    resultReroad (newColor, newSecret) //正解数反映
+    reflectionColor(newColor, newSecret);//色移動反映
   });
   }
